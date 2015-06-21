5c48feaaee97daaea414943d2f6a2db2132b58e8
marjuk <marjuk@stanford.edu>
2015-06-21 00:33:51 -0700

    Update main.js

--- main.js
+++ main.js
@@ -9,28 +9,28 @@ target = $( '#wisdom-container' )[0];
     i = 0;
     messages = [
       
-        '<div class="animated fadeInUp"> Fucking Schedule Tasks Like College Classes. </div>',
-        '<div class="animated fadeIn"> Fucking Delete the Task. Do not Reschedule It.  </div>', 
-        '<div class="animated fadeIn"> Avoid Fucking Distractions.  </div>',
+        '<div class="animated fadeInUp">  Schedule Tasks Like College Classes. </div>',
+        '<div class="animated fadeIn"> Delete the Task. Do not Reschedule It.  </div>', 
+        '<div class="animated fadeIn"> Avoid Distractions.  </div>',
         '<div class="animated fadeIn"> Never Ever Answer the Phone Unless You Are Expecting The Call.  </div>',
-        '<div class="animated fadeIn"> Stay Fucking On Top of Your Health. </div>',
-        '<div class="animated fadeIn"> Fucking Have a Morning Brain Purge. </div>', 
-        '<div class="animated fadeIn"> Create a Routine and Fucking Follow it. </div>', 
-        '<div class="animated fadeIn"> Always Set Fucking Deadlines. </div>', 
-        '<div class="animated fadeIn"> Set a Specific Time Of the Day to Check Your Fucking Email. </div>', 
-        '<div class="animated fadeIn"> Turn Off Fucking Email Notifications </div>', 
-        '<div class="animated fadeIn"> Fucking Batch Your Tasks. </div>', 
+        '<div class="animated fadeIn"> Stay On Top of Your Health. </div>',
+        '<div class="animated fadeIn"> Have a Morning Brain Purge. </div>', 
+        '<div class="animated fadeIn"> Create a Routine and  Follow it. </div>', 
+        '<div class="animated fadeIn"> Always Set  Deadlines. </div>', 
+        '<div class="animated fadeIn"> Set a Specific Time Of the Day to Check Your Email. </div>', 
+        '<div class="animated fadeIn"> Turn Off Email Notifications </div>', 
+        '<div class="animated fadeIn">  Batch Your Tasks. </div>', 
         '<div class="animated fadeIn"> Create a Weekly Strategy Document. </div>',
-        '<div class="animated fadeIn"> Get to Inbox Zero Fucking Every Day. </div>',
+        '<div class="animated fadeIn"> Get to Inbox Zero  Every Day. </div>',
         '<div class="animated pulse"> Learn to Say Fucking NO. </div>',
-        '<div class="animated fadeIn"> Do the One Fucking Thing that Would Make You Satisfied with Your Day. </div>', 
-        '<div class="animated fadeIn"> Again Fucking Priorotize.  </div>',
-        '<div class="animated fadeIn"> Follow the Fucking Pomodoro Technique, and Move around at least Once an Hour to Get the Fucking Blood Flowing. </div>',
+        '<div class="animated fadeIn"> Do the One Thing that Would Make You Satisfied with Your Day. </div>', 
+        '<div class="animated fadeIn"> Again Priorotize.  </div>',
+        '<div class="animated fadeIn"> Follow the Pomodoro Technique, and Move around at least Once an Hour to Get the Fucking Blood Flowing. </div>',
         '<div class="animated fadeIn"> Run Every Task through an Impact vs. Effort Analysis. </div>',
-        '<div class="animated pulse"> Never, Ever Fucking Proctastinate.   </div>',
-        '<div class="animated fadeIn"> Fucking Work Even When You Do Not Feel Like It. </div>',
-        '<div class="animated fadeIn"> Journal Daily, Even When It Seems Like There is No Fucking Time. </div>', 
-        '<div class="animated fadeIn"> Set Fucking Activities Outside of Work. </div>'
+        '<div class="animated pulse"> Never, Ever Proctastinate.   </div>',
+        '<div class="animated fadeIn"> Work Even When You Do Not Feel Like It. </div>',
+        '<div class="animated fadeIn"> Journal Daily, Even When It Seems Like There is No Time. </div>', 
+        '<div class="animated fadeIn"> Set Activities Outside of Work. </div>'
 
         
 
